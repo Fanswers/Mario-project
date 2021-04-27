55d8b4201fb79d00f65384e2c6fab5f408042002
Jucrz <juliencruz78@gmail.com>
2021-04-27 23:41:39 +0200

    Detection touches

--- myscene.cpp
+++ myscene.cpp
@@ -5,15 +5,14 @@ myScene::myScene() : QWidget()
 {
     this -> setFixedSize(1500,1000);
 
-    fond = new QRect(100,100,100,100);
-
 
     QPixmap bkgnd(QCoreApplication::applicationDirPath() + "/../../mario-project/Img/Map.png");
     bkgnd = bkgnd.scaled(15000,1000);
 
     //Décalage du background
     QRegion exposed;
-    bkgnd.scroll(-10000, 0, bkgnd.rect(), &exposed);
+    int x = 0;
+    bkgnd.scroll(x, 0, bkgnd.rect(), &exposed);
 
     QPalette palette;
     palette.setBrush(QPalette::Window, bkgnd);
@@ -27,3 +26,29 @@ myScene::myScene() : QWidget()
 
     QObject::connect(m_bouton, SIGNAL(clicked()), m_bouton, SLOT(close()));
 }
+
+
+
+bool myScene::event(QEvent* event) {
+    if (event->type()==QEvent::KeyPress) {
+        QKeyEvent* key = static_cast<QKeyEvent*>(event);
+        if (key->key()==Qt::Key_Z) {
+            qDebug() << "Haut";
+        } else if (key->key()==Qt::Key_Q) {
+            qDebug() << "Gauche";
+        } else if (key->key()==Qt::Key_S) {
+            qDebug() << "Bas";
+        } else if (key->key()==Qt::Key_D) {
+            qDebug() << "Droite";
+        }
+        else {
+            return QWidget::event(event);
+        }
+        return true;
+    } else {
+        return QWidget::event(event);
+    }
+    return false;
+}
+
+
--- myscene.h
+++ myscene.h
@@ -5,15 +5,26 @@
 #include <QWidget>
 #include <QPushButton>
 #include <QApplication>
+#include <QDebug>
+#include <QEvent>
+#include <QKeyEvent>
 
 class myScene : public QWidget
 {
 public:
     myScene();
+    void setIsTowardLeft(bool b) {isTowardLeft = b;}
+    void setIsTowardRight(bool b) {isTowardRight = b;}
+    bool event(QEvent* event);
 
 private:
     QPushButton *m_bouton;
-    QRect *fond;
+    bool isTowardLeft;
+    bool isTowardRight;
+
+protected:
+
+
 };
 
 #endif // MYSCENE_H
